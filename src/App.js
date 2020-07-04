f52886694efff6bf31d207d275daa226139c8742
yakubu rufai mahatma <yrufai23@gmail.com>
2020-07-04 01:38:33 +0000

    updated items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, } from "react-router-dom"
+
+
+
 import HomePage from "./pages/homepage/homepage.component";
 import './App.css';
 
 function App() {
   return (
-    <div >
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+    </Switch>
   );
 }
 
