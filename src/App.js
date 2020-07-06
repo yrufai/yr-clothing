260246b59fcd60824db80e8a5c96e3fbec402d97
yakubu rufai mahatma <yrufai23@gmail.com>
2020-07-06 22:54:06 +0000

    header section added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import { BrowserRouter as Router, Route, Link, Switch, } from "react-router-dom"
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+
+
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-    </Switch>
-  );
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
