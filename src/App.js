b1ff108bec81bca4f8225bb729b30948c085711b
yakubu rufai mahatma <yrufai23@gmail.com>
2020-07-14 22:55:12 +0000

    sign in page created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, } from "react-router-dom"
-
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInandSignUpPage from "./pages/signIn-signUp/signIn-signUp.component";
 
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -17,9 +15,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInandSignUpPage} />
       </Switch>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/pages/signIn-signUp/signIn-signUp.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SignIn from "../../components/signIn/signIn.component";
+
+import "./signIn-signUp.style.scss";
+
+const SignInandSignUpPage = () => (
+  <div className="signIn-signUp">
+    <SignIn />
+  </div>
+);
+
+export default SignInandSignUpPage;
